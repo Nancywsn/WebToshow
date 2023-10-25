8383b5ae88e81c42f66b65bb71f919398db32224
Nancy Wang <2364132740@qq.com>
2023-10-25 21:15:10 +0800

    homepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import toshow from '../views/toshow.vue'
+import home from '../views/home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,6 +8,11 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      component: home
+    },
+    {
+      path: '/result',
+      name: 'result',
       component: toshow
     },
   ]
---
+++ src/views/home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="wrapper">
+        <div id="bg"></div>
+        <div id="overlay"></div>
+        <div id="main">
+            <!-- Header -->
+            <header id="header">
+                <h1>雪地搜救</h1>
+                <p>&nbsp;&bull;&nbsp; 无人机小目标检测 &nbsp;&bull;&nbsp;</p>
+                <nav>
+                    <el-link href="/result" >Start</el-link>
+                </nav>
+            </header>
+            <!-- Footer -->
+            <footer id="footer">
+                <span class="copyright"> 2023 小组丨华中科技大学</span>
+            </footer>
+        </div>
+    </div>
+</template>
+  
+<script>
+
+</script>
+  
+<style lang="scss" scoped>
+
+@import "../assets/css/main.css";
+
+</style>
+
+  
