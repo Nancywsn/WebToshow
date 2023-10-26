8b346e12afea44691e4116123d8ef313b344a973
sleepylyx <1287003026@qq.com>
2023-10-26 20:51:34 +0800

    10.26

--- src/views/toshow.vue
+++ src/views/toshow.vue
@@ -8,74 +8,98 @@
                 </b>
             </el-header>
             <el-main>
-                <div class="choose">
-                   <input type="file" style="font-family: 楷体; display: inline-block; align-self: center;" @change="handleFileChange" accept="image/*">
-                </div>
-                <div>
-                    <el-row class="horizontal-row">
-                        <el-col style="width: 400px; display: flex; align-items: center; justify-content: center;">
-                            <el-card shadow="hover"
-                                style="display: flex; flex-direction: row; align-items: center; justify-content: center; height: 100%;">
-                                <div v-if="selectedImage">
+              <div>
+                <el-row class="horizontal-row2">
+                  <el-col class="jianxie">
+                    <div>
+                      <p class="display">输 入 图 片
+                      </p>
+                    </div>
+                  </el-col>
+                  <el-col style="width: 100px;">
+                  </el-col>
+                  <el-col class="jianxie">
+                    <p class="display">     输 出 图 片
+                    </p>
+                  </el-col>
+                  <el-col class="jianxie">
+                    <p class="display">     识 别 数 据
+                    </p>
+                  </el-col>
+                </el-row>
+              </div>
+              <div class="main">
+                <el-row>
+                  <div class="choose">
+                     <input type="file" style="font-family: 楷体; display: inline-block; align-self: center;" @change="handleFileChange" accept="image/*">
+                  </div>
+                </el-row>>
+                  <div>
+                      <el-row class="horizontal-row">
+                          <el-col style="width: 400px; display: flex; align-items: center; justify-content: center;">
+                              <el-card shadow="hover"
+                                  style="display: flex; flex-direction: row; align-items: center; justify-content: center; height: 100%;">
+                                  <div v-if="selectedImage">
 
-                                    <!--      显示图片     -->
-                                    <img :src="selectedImage" alt="Selected Image"
-                                        style="width: 300px; height: auto; max-height: 400px;" />
-                                </div>
-                            </el-card>
-                        </el-col>
-                        <el-col
-                            style="width: 100px; height: 400px; display: flex; flex-direction: column; align-items: center; justify-content: center;"
-                            class="mb-4">
-                          <el-row style="width: 100px; height: 30px; display: flex; align-items: center; justify-content: center;">
-                            <el-button size="large" class="button" @click="buttonClick" style="font-family: 楷体;"
-                                round>识别</el-button>
-                          </el-row>
-                          <el-row style="width: 100px; height: 60px; display: flex; align-items: center; justify-content: center;">
-                                <img src= "src/assets/arrow55.png" alt="" style="opacity: 0.8; width: 100px; height: 50px;"/>
-                          </el-row>
-                        </el-col>
+                                      <!--      显示图片     -->
+                                      <img :src="selectedImage" alt="Selected Image"
+                                          style="width: 300px; height: auto; max-height: 400px;" />
+                                  </div>
+                              </el-card>
+                          </el-col>
+                          <el-col
+                              style="width: 100px; height: 400px; display: flex; flex-direction: column; align-items: center; justify-content: center;"
+                              class="mb-4">
+                            <el-row style="width: 100px; height: 30px; display: flex; align-items: center; justify-content: center;">
+                              <el-button size="large" class="button" @click="buttonClick" style="font-family: 楷体;"
+                                  round>识别</el-button>
+                            </el-row>
+                            <el-row style="width: 100px; height: 60px; display: flex; align-items: center; justify-content: center;">
+                                  <img src= "src/assets/arrow55.png" alt="" style="opacity: 0.8; width: 100px; height: 50px;"/>
+                            </el-row>
+                          </el-col>
 
-                        <el-col style="width: 400px; display: flex; align-items: center; justify-content: center;">
-                            <el-card shadow="hover"
-                                style="width: 400px; display: flex; align-items: center; justify-content: center;">
-                                <img :src="codeUrl" style=" max-width: 300px; height: auto; max-height: 400px;" />
-                            </el-card>
-                        </el-col>
+                          <el-col style="width: 400px; display: flex; align-items: center; justify-content: center;">
+                              <el-card shadow="hover"
+                                  style="width: 400px; display: flex; align-items: center; justify-content: center;">
+                                  <img :src="codeUrl" style=" max-width: 300px; height: auto; max-height: 400px;" />
+                              </el-card>
+                          </el-col>
 
-                        <el-col style=" width: 300px; height: 400px; display: flex; flex-direction: column; align-items: center; ">
-                            <p class="data1" style="font-family: 楷体;">
-                                经过识别，图片中
-                            </p>
-                            <p class="data1" style="font-family: 楷体;">
-                               识别到人员总数：{{peoNum}}
-                            </p>
-                          <div style="width: 250px; height: 50px; display: flex; align-items: center; flex-direction: row;">
-                            <el-col>
-                              <p class="data2" style="font-family: 楷体;">
-                                是否投放救生包:
+                          <el-col style=" width: 300px; height: 400px; display: flex; flex-direction: column; align-items: center; ">
+                              <p class="data1" style="font-family: 楷体;">
+                                  经过识别，图片中
                               </p>
-                            </el-col>
-                            <el-col>
-                                <el-button size="large" class="button" @click="buttonClick" style="font-family: 楷体;"
-                                round>投放</el-button>
-                            </el-col>
-                          </div>
-                          <div style="width: 250px; height: 50px; display: flex; align-items: center; flex-direction: row;">
-                            <el-col>
-                              <p class="data2" style="font-family: 楷体;">
-                                是否请求更多相关图片:
+                              <p class="data1" style="font-family: 楷体;">
+                                 识别到人员总数：{{peoNum}}
                               </p>
-                            </el-col>
-                            <el-col>
-                                <el-button size="large" class="button" @click="buttonClick" style="font-family: 楷体;"
-                                round>请求</el-button>
-                            </el-col>
-                          </div>
+                            <div style="width: 250px; height: 50px; display: flex; align-items: center; flex-direction: row;">
+                              <el-col>
+                                <p class="data2" style="font-family: 楷体;">
+                                  是否投放救生包:
+                                </p>
+                              </el-col>
+                              <el-col>
+                                  <el-button size="large" class="button" @click="buttonClick" style="font-family: 楷体;"
+                                  round>投放</el-button>
+                              </el-col>
+                            </div>
+                            <div style="width: 250px; height: 50px; display: flex; align-items: center; flex-direction: row;">
+                              <el-col>
+                                <p class="data2" style="font-family: 楷体;">
+                                  是否请求更多相关图片:
+                                </p>
+                              </el-col>
+                              <el-col>
+                                  <el-button size="large" class="button" @click="buttonClick" style="font-family: 楷体;"
+                                  round>请求</el-button>
+                              </el-col>
+                            </div>
 
-                        </el-col>
-                    </el-row>
-                </div>
+                          </el-col>
+                      </el-row>
+                  </div>
+              </div>
             </el-main>
         </el-container>
     </div>
@@ -135,7 +159,7 @@ const buttonClick = function () {
 <style scoped lang="scss">
 // @import "../assets/css/main.css";
 .header {
-    font-size: 70px;
+    font-size: 60px;
     /* 你可以根据需要调整字体大小，单位可以是像素(px)、em等 */
 }
 
@@ -146,9 +170,45 @@ const buttonClick = function () {
     //opacity:0.6;
 }
 
-.el-main {
+.main {
     background-color: rgba(255, 255, 255, 0.7);
 }
+
+.display{
+  display:flex;
+  font-size: 20px;
+  //position: relative;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  line-height: 30px;
+  font-family: 楷体;
+
+}
+
+.jianxie {
+  height: 35px;
+  width: 200px;
+  background-color: rgba(255, 255, 255, 0.7);
+  border-left-color: white;
+  border-right-color: white;
+  border-top-color: white;
+  border-width: 1px; /* 设置边框宽度 */
+  border-style: solid; /* 设置边框样式 */
+
+  /* 简写四个字表示左上角、右上角、右下角、左下角 */
+  /* border-radius: 10px 20px 30px 40px; */
+  /* 简写两个值，第一个表示左上角、右下角，第二个值不表示右上角、左下角； */
+  /* border-radius: 10px 40px; */
+  /* 简写三个值，第一个值表示左上角，第二个值表示右上角、左下角，第三个值表示右下角 */
+  /* border-radius: 10px 20px 30px; */
+  /* 一个值表示四个角 */
+  border-radius: 10px 10px 0px 0px;
+  border-bottom: none;
+  margin-left: 100px;
+  margin-right: 100px;
+}
+
 
 .button {
     font-size: 17px
@@ -159,6 +219,9 @@ const buttonClick = function () {
     /* 使用 flex 布局来实现水平排列 */
     justify-content: space-between;
     /* 在 el-col 之间添加空白间隔 */
+}
+.horizontal-row2{
+  display: flex;
 }
 
 .choose{
